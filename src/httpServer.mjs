ff9680b11f003f42ccea1ff0abec99a1ed07f8c2
Кирилл U10 <u10110.uu@gmail.com>
2026-06-19 18:40:15 +0300

    1 pack updates

--- src/httpServer.mjs
+++ src/httpServer.mjs
@@ -394,7 +394,9 @@ async function handleRadioUploadRequest({ req, res, pathname, repo, jwtSecret, e
 }
 
 async function handleProfileImageUploadRequest({ req, res, pathname, repo, jwtSecret, env }) {
+  
   if (pathname !== PROFILE_IMAGE_UPLOAD_ENDPOINT) {
+    sendJson(res, 401, { error: pathname });
     return false;
   }
 
