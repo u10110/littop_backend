54cf0310e14c0a95840bd3c2f40601b0587de645
Кирилл U10 <u10110.uu@gmail.com>
2026-06-20 15:05:07 +0300

    fix serv path

--- src/httpServer.mjs
+++ src/httpServer.mjs
@@ -631,7 +631,7 @@ export function createHttpServer({ apolloServer, repo, jwtSecret, adminUserIds =
         return;
       }
 
-      if (pathname === '/' || pathname === '/graphql') {
+      if (pathname === '/api' || pathname === '/graphql') {
         await handleGraphqlRequest({ req, res, apolloServer, repo, jwtSecret, adminUserIds });
         return;
       }
